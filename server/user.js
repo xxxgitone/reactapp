f08254903d6808bb7963e349bf05120353f59edd
xxxgitone <xxxgit@sina.com>
2018-03-19 23:32:43 +0800

    server

--- server/user.js
+++ server/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const utils = require('utility')
 const model = require('./model')
 const User = model.getModel('user')
 
@@ -18,16 +19,74 @@ router.post('/register', (req, res) => {
         msg: '用户名重复'
       })
     }
-    User.create({user, pwd, type}).then(data => {
+
+    const userModel = new User({
+      user,
+      type,
+      pwd: md5Pwd(pwd)
+    })
+
+    userModel.save().then(data => {
+      const { user, type, _id } = data
+      res.cookie('userid', _id)
       return res.json({
-        code: 0
+        code: 0,
+        data: {
+          user,
+          type,
+          _id
+        }
+      })
+    }).catch(e => {
+      return res.json({
+        code: 1,
+        msg: '后台出错了'
       })
     })
   })
 })
 
-router.get('/info', (req, res) => {
-  return res.json({code: 1})
+router.post('/login', (req, res) => {
+  const { user, pwd } = req.body
+  User.findOne({user, pwd: md5Pwd(pwd)}, {pwd: 0}).then(data => {
+    if (data) {
+      // 设置cookie
+      res.cookie('userid', data._id)
+      return res.json({
+        code: 0,
+        data
+      })
+    } else {
+      return res.json({
+        code: 1,
+        msg: '用户名不存在或者密码错误'
+      })
+    }
+  })
 })
+
+router.get('/info', (req, res) => {
+  const { userid } = req.cookies
+  if (!userid) {
+    return res.json({code: 1})
+  }
+  User.findOne({_id: userid}, {pwd: 0}).then(data => {
+    return res.json({
+      code: 0,
+      data
+    })
+  }).catch(e => {
+    return res.json({
+      code: 1,
+      msg: '后台出错了'
+    })
+  })
+})
+
+function md5Pwd (pwd) {
+  const salt = '_md5_salt_'
+  // 加盐,并且两层md5处理
+  return utils.md5(utils.md5(pwd + salt))
+}
 
 module.exports = router
