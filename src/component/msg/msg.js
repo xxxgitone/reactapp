61db4260e434d9c3578098a7a787ee5df11fafe1
xxxgitone <xxxgit@sina.com>
2018-04-19 23:06:41 +0800

    learn react+redux

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {
-  List
+  List,
+  Badge
 } from 'antd-mobile'
 import { connect } from 'react-redux'
 
@@ -21,24 +22,37 @@ class Msg extends Component {
       msgGroup[v.chatid].push(v)
     })
 
-    const chatList = Object.values(msgGroup)
     const userid = this.props.user._id
     const userinfo = this.props.chat.users
+
+    const chatList = Object.values(msgGroup).sort((a, b) => {
+      const a_last = this.getLastMsg(a).create_time
+      const b_last = this.getLastMsg(b).create_time
+      return b_last - a_last
+    })
+
     return (
       <div>
         {
           chatList.map(v => {
             const lastMsg = this.getLastMsg(v)
             const targetId = v[0].from === userid ? v[0].to : v[0].from
-            const name = userinfo[targetId] && userinfo[targetId].name
-            const avatar = userinfo[targetId] && userinfo[targetId].avatar
+            const unreadNum = v.filter(v => !v.read && v.to === userid).length
+            if (!userinfo[targetId]) {
+							return null
+						}
             return (
               <List>
                 <List.Item
                   key={lastMsg._id}
-                  thumb={require(`../img/${avatar}.png`)}>
+                  extra={<Badge text={unreadNum}></Badge>}
+                  thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
+                  arrow="horizontal"
+                  onClick={() => {
+                    this.props.history.push(`/chat/${targetId}`)
+                  }}>
                   {lastMsg.content}
-                  <List.Item.Brief>{name}</List.Item.Brief>
+                  <List.Item.Brief>{userinfo[targetId].name}</List.Item.Brief>
                 </List.Item>
               </List>
             )
